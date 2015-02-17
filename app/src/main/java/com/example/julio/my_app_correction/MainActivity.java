8d3946b130d9c2ae38841060208fa05c859b1dfc
juliocsar13 <jc_csar_15@hotmail.com>
2015-02-16 20:01:53 -0500

    musica en android studio

--- app/src/main/java/com/example/julio/my_app_correction/MainActivity.java
+++ app/src/main/java/com/example/julio/my_app_correction/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.julio.my_app_correction;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+  //  private MediaPlayer np;
+  //  private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+   //     btnRegister = (Button) findViewById(R.id.btnRegister);
+   //     btnRegister.setOnClickListener(new View.OnClickListener(){
+   //         @Override
+   //         public void onClick(View view){
+   //             np = MediaPlayer.create(MainActivity.this,R.raw.julio);
+   //             np.start();
+   //         }
+
+   //     });
 
     }
     public void registro(View view)
