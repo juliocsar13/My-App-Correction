4e17be689087c1a8a1a07987f537e4d7dfa8263b
juliocsar13 <jc_csar_15@hotmail.com>
2015-02-20 19:51:21 -0500

    responsive desing  para activity main 4 trabajando con dimensiones  estilos D:

--- app/src/main/java/com/example/julio/my_app_correction/MainActivity6.java
+++ app/src/main/java/com/example/julio/my_app_correction/MainActivity6.java
@@ -1,48 +1,24 @@
 package com.example.julio.my_app_correction;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
-
-    private MediaPlayer np;
-    private Button btnRegister;
+public class MainActivity6 extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity6);
-
-
-    }
-    public void registro(View view)
-    {
-       Intent registros = new Intent(this,MainActivity2.class);
-       startActivity(registros);
-       btnRegister = (Button) findViewById(R.id.btnRegister);
-       btnRegister.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view){
-                np = MediaPlayer.create(MainActivity.this,R.raw.julio);
-                np.start();
-            }
-        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity6, menu);
         return true;
     }
 
