3baefe0508cf653333ed6b13aef8c66dc9e0154e
juliocsar13 <jc_csar_15@hotmail.com>
2015-02-24 20:00:55 -0500

    responsive desing  para activity main 4 trabajando con dimensiones  estilos D:

--- app/src/main/java/com/example/julio/my_app_correction/MainActivity.java
+++ app/src/main/java/com/example/julio/my_app_correction/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity6);
+        setContentView(R.layout.activity_main_activity7);
 
 
     }
